4017a175fb77c699a51fcf4b5f632911a68dba4a
Telectroboy <telectroboy@hotmail.com>
2019-12-16 08:27:26 +0100

    Update main.cpp

--- TTN_TTGOLora32_OTAA/src/main.cpp
+++ TTN_TTGOLora32_OTAA/src/main.cpp
@@ -26,17 +26,17 @@ SSD1306 display (OLED_I2C_ADDR, OLED_SDA, OLED_SCL);
 // the bytes.
 
 // Copy the value from Device EUI from the TTN console in LSB mode.
-static const u1_t PROGMEM DEVEUI[8]= { ... };
+static const u1_t PROGMEM DEVEUI[8]= { 0x57, 0x7A, 0x57, 0x6F, 0x3F, 0xD8, 0xC0, 0x00 };
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 
 // Copy the value from Application EUI from the TTN console in LSB mode
-static const u1_t PROGMEM APPEUI[8]= { ... };
+static const u1_t PROGMEM APPEUI[8]= { 0xC2, 0x60, 0x01, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is. Anyway its in MSB mode.
-static const u1_t PROGMEM APPKEY[16] = { ... };
+static const u1_t PROGMEM APPKEY[16] = { 0x5D, 0x31, 0xE1, 0xD6, 0x34, 0xD4, 0x38, 0x87, 0x9B, 0x07, 0x44, 0x94, 0x8D, 0x17, 0xAA, 0x39 };
 void os_getDevKey (u1_t* buf) { memcpy_P(buf, APPKEY, 16);}
 
 static osjob_t sendjob;
@@ -55,19 +55,20 @@ const lmic_pinmap lmic_pins = {
 
 void do_send(osjob_t* j){
     // Payload to send (uplink)
-    static uint8_t message[] = "Hello OTAA!";
+    static uint8_t message[] = "Hello Manu!";
 
     // Check if there is not a current TX/RX job running
     if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
+        Serial.println(F("Job de TX ou RX en cours, pas d'émission !"));
     } else {
         // Prepare upstream data transmission at the next possible time.
         LMIC_setTxData2(1, message, sizeof(message)-1, 0);
-        Serial.println(F("Sending uplink packet..."));
+        Serial.println(F("Envoi d'un paquet..."));
         digitalWrite(LEDPIN, HIGH);
         display.clear();
-        display.drawString (0, 0, "Sending uplink packet...");
-        display.drawString (0, 50, String (++counter));
+        display.drawString (0, 0, "Envoi d'un paquet...");
+        display.drawString (0, 50, "Compteur");
+        display.drawString (60, 50, String (++counter));
         display.display ();
     }
     // Next TX is scheduled after TX_COMPLETE event.
@@ -78,49 +79,55 @@ void onEvent (ev_t ev) {
     Serial.print(": ");
     switch(ev) {
            case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            Serial.println(F("EV_TXCOMPLETE (inclus temps d'attente fenêtre RX)"));
             display.clear();
-            display.drawString (0, 0, "EV_TXCOMPLETE event!");
+            display.drawString (0, 0, "Paquet émis !");
 
             if (LMIC.txrxFlags & TXRX_ACK) {
-              Serial.println(F("Received ack"));
-              display.drawString (0, 20, "Received ACK.");
+              Serial.println(F("Accusé reçu"));
+              display.drawString (0, 20, "Accusé reçu.");
             }
 
             if (LMIC.dataLen) {
               int i = 0;
               // data received in rx slot after tx
-              Serial.print(F("Data Received: "));
+              Serial.print(F("Données Recues: "));
               Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
               Serial.println();
+              Serial.println(LMIC.snr);
               Serial.println(LMIC.rssi);
 
-              display.drawString (0, 9, "Received DATA.");
+              display.drawString (0, 9, "Signaux TTGO => GW:");
               for ( i = 0 ; i < LMIC.dataLen ; i++ )
                 TTN_response[i] = LMIC.frame[LMIC.dataBeg+i];
               TTN_response[i] = 0;
-              display.drawString (0, 22, String(TTN_response));
-              display.drawString (0, 32, String(LMIC.rssi));
-              display.drawString (64,32, String(LMIC.snr));
+              display.drawString (0, 20, String(TTN_response));
+
+              display.drawString (0, 32, "Signaux GW => TTGO");
+              display.drawString (0, 42, "RSSI:");
+              display.drawString (27, 42, String(LMIC.rssi));
+              display.drawString (50, 42, "SNR:");
+              display.drawString (76, 42, String(LMIC.snr / 4));
             }
 
             // Schedule next transmission
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
             digitalWrite(LEDPIN, LOW);
-            display.drawString (0, 50, String (counter));
+            display.drawString (0, 50, "Compteur");
+            display.drawString (60, 50, String (counter));
             display.display ();
             // Schedule next transmission
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
             break;
         case EV_JOINING:
-            Serial.println(F("EV_JOINING: -> Joining..."));
-            display.drawString(0,16 , "OTAA joining....");
+            Serial.println(F("EV_JOINING: -> Activation OTA"));
+            display.drawString(0,16 , "Activation OTA");
             display.display();
             break;
         case EV_JOINED: {
               Serial.println(F("EV_JOINED"));
               display.clear();
-              display.drawString(0 , 0 ,  "Joined!");
+              display.drawString(0 , 0 ,  "Connecté!");
               display.display();
               // Disable link check validation (automatically enabled
               // during join, but not supported by TTN at this time).
@@ -147,7 +154,7 @@ void onEvent (ev_t ev) {
 void setup() {
     Serial.begin(115200);
     delay(2500);                      // Give time to the serial monitor to pick up
-    Serial.println(F("Starting..."));
+    Serial.println(F("Démarrage..."));
 
     // Use the Blue pin to signal transmission.
     pinMode(LEDPIN,OUTPUT);
@@ -164,8 +171,12 @@ void setup() {
 
     display.setTextAlignment (TEXT_ALIGN_LEFT);
 
-    display.drawString (0, 0, "Starting....");
+    display.drawString (0, 0, "Démarrage....");
+    display.drawString (0, 12, "ID-REP Electronic");
+    display.drawString (0, 30, "BILLAUDEL JP - 12/2019");
+
     display.display ();
+    delay(2000);
 
     // LMIC init
     os_init();
@@ -200,11 +211,11 @@ void setup() {
     LMIC_setLinkCheckMode(0);
 
     // TTN uses SF9 for its RX2 window.
-    LMIC.dn2Dr = DR_SF9;
+    LMIC.dn2Dr = DR_SF11;
 
     // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
-    //LMIC_setDrTxpow(DR_SF11,14);
-    LMIC_setDrTxpow(DR_SF9,14);
+    LMIC_setDrTxpow(DR_SF11,14);
+    //LMIC_setDrTxpow(DR_SF9,14);
 
     // Start job
     do_send(&sendjob);     // Will fire up also the join
